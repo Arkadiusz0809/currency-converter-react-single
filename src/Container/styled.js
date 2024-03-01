fdd90d69ae70e38d896f03a7605943d5636b0f17
Arkadiusz Torun <arek.torun12@gmail.com>
2024-03-01 16:12:55 +0100

    decreased padding

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.main`
     display: flex;
+    flex-wrap: wrap;
     flex-basis: 600px;
     box-shadow: 1px 2px 1px 2px teal;
     margin: 20px;
-    padding: 30px 30px 0px 30px;
+    padding: 10px 10px 0px 10px;
     background-color: white;
     justify-content: center;
     border-radius: 10px;
